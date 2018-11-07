c04f375c100755def73aabc35bf80218d6110546
Dominik Schopper <dominik.schopper@bisnode.com>
2018-11-07 18:36:59 +0100

    put example data source into App

--- project/src/App.js
+++ project/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
 
 import Autoc from './containers/autoc/autoc';
+import countriesArray from "./example-data/countries-array";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="eight columns bg-white" >
-          <Autoc name="example 1"></Autoc>
+          <Autoc name="example 1" minLength="2" source={countriesArray} />
         </div>
       </div>
     );
