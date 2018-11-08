7e4c1e532d5bbe079f9333ee4a89df1ea8a76ac3
Dominik Schopper <dominik.schopper@bisnode.com>
2018-11-08 09:47:04 +0100

    adding different source data

---
+++ project/src/example-data/languages-array.js
@@ -0,0 +1,7 @@
+const programmingLanguages = [
+    'Javascript', 'C++', 'C#', 'Java', 'Python', 'Perl', 'Lua', 'Scala',
+    'Lisp', 'C', 'B', 'Cobol', 'Fortran', 'Basic', 'VisualBasic',
+    'Brainfuck'
+];
+
+export default programmingLanguages;
